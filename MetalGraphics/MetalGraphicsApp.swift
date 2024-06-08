f653a8706c97f7f40fb7d457bdd3fc8faf8bdb5a
mykola.diahovets <mykola.diahovets@digicert.com>
2024-06-08 14:39:23 +0300

    Initial Commit

---
+++ MetalGraphics/MetalGraphicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetalGraphicsApp.swift
+//  MetalGraphics
+//
+//  Created by Nikolay Diahovets on 08.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct MetalGraphicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
